d43d3c2fdc7babba8b6e7f517363e3bdae6badf2
nandinirakas <nandinirak@thelaunchclub.in>
2022-02-28 18:59:01 +0530

    feature.xml

---
+++ child1/src/main/java/com/thelaunchclub/child1/Addition.java
@@ -0,0 +1,18 @@
+package com.thelaunchclub.child1;
+
+import java.util.Scanner;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class Addition {
+    
+    public static int add() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number for addition");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number for addition");
+        int b = scanner.nextInt();
+        return a + b;
+    }
+}
---
+++ child1/src/main/java/com/thelaunchclub/child1/Subtraction.java
@@ -0,0 +1,18 @@
+package com.thelaunchclub.child1;
+
+import java.util.Scanner;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class Subtraction {
+    
+    public static int sub() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number for subtraction");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number for subtraction");
+        int b = scanner.nextInt();
+        return a - b;
+    }
+}
