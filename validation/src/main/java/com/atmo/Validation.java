c6e83ad57f3b1f8eb6ccf3a9f70669fe6de25756
nandinirakas <nandinirak@thelaunchclub.in>
2022-03-07 17:03:03 +0530

    validation

---
+++ validation/src/main/java/com/atmo/Validation.java
@@ -0,0 +1,15 @@
+package com.atmo;
+
+public interface Validation {
+    boolean employeeIdValidation(final String employeeId);
+
+    boolean employeeNameValidation(final String employeeName);
+
+    boolean phoneNumberValidation(final String phoneNumber);
+
+    boolean dateValidation(final String joiningDate);
+
+    boolean employeeSalaryValidation(final String employeeSalary);
+
+    boolean validateChoice(final String choice);
+}
